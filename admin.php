4bab76b4c3cc6f6112ef59c13c7a1bf1dce15dfc
kimyh <mk.kimyh@gmail.com>
2018-12-06 18:23:21 +0900

    인트로 페이지 수정 사항 적용 완료

---
+++ admin.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>빌라드지디더그레이스켈리</title>
+        <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/assets/css/admin.css">
+    </head>
+    <body>
+        <main id="admin-intro">
+            <article>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <h1 class="logo">
+                                <img src="/assets/images/intro/admin_logo.png" class="img-responsive" alt="">
+                            </h1>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="gangnam">
+                                <figure>
+                                    <img src="/assets/images/intro/img_btn_gangnam.png" alt="강남점">
+                                    <figcaption>
+                                        <div class="btn-area">
+                                            <a href="http://gangnam.thegracekelly1.com/manager/" class="btn btn-default" role="button">관리자 바로가기</a>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        </div>
+                        <div class="col-xs-6">
+                            <div class="suseo">
+                                <figure>
+                                    <img src="/assets/images/intro/img_btn_suseo.png" alt="수서점">
+                                    <figcaption>
+                                        <div class="btn-area">
+                                            <a href="javascript:window.alert('준비중입니다.');" class="btn btn-default" role="button">관리자 바로가기</a>
+                                        </div>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </main>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
